70cf7f37bee27923f943a108670a7f11d2ecae5b
MooleSreenu512 <moolesreenu512@gmail.com>
2020-02-07 11:51:41 +0530

    dd

---
+++ sum.java
@@ -0,0 +1,16 @@
+public class one{
+	public static void main(String[] args){
+		int a = 10;
+		int b = 15;
+		int c = 20;
+		int a = a+b+c;
+		system.out.println(a);
+	}
+}
+:wq
+:w
+:wq
+
+:w
+:wq
+
